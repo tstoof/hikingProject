ecfdc8169f6f57b7e6e4b1e465e25946ca22a2f4
tamara <tstoof@sogyo.nl>
2024-12-20 14:40:42 +0100

    deleted useless page

--- hikingApp/hike/urls.py
+++ hikingApp/hike/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="routeplanner"),
-    path("map/", views.map, name="map")
+    path("", views.index, name="routeplanner")
     ]
