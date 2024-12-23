eabda7e4312d91bc96bc4575812d3db081d68eaa
tamara <tstoof@sogyo.nl>
2024-12-23 11:39:36 +0100

    better folder structure

--- hike/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("routeplanner/", views.routeplanner, name="routeplanner"),
-    path("", views.frontpage, name="frontpage")
-    ]
